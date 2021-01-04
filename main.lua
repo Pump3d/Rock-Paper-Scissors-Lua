333175fecd17532a5eec83d4ceedbbcfa2166e16
Pump3d <poopeepoopeeohyeah@gmail.com>
2021-01-04 21:55:51 +0000

    Added a score display as well as some information.

--- main.lua
+++ main.lua
@@ -4,12 +4,28 @@ math.randomseed(os.time())
 
 local start = 1
 local played = 0
+local info = 0
 
 local scores = {
   playerScore = 0,
   aiScore = 0
 }
 
+local function displayScores()
+	if scores.playerScore == scores.aiScore then
+		if scores.playerScore == 1 then
+			io.write("You and the AI both have " .. scores.playerScore .. " point.\n")
+		else
+			io.write("You and the AI both have " .. scores.playerScore .. " points. \n")
+		end
+	elseif scores.playerScore ~= 1 and scores.aiScore == 1 then
+		io.write("You have " .. scores.playerScore .. " points, and the AI has " .. scores.aiScore .. " point. \n")
+	elseif playerScore == 1 and aiScore ~= 1 then
+		io.write("You have " .. scores.playerScore .. " point, and the AI has " .. scores.aiScore .. " points. \n")
+	elseif playerScore ~= 1 and aiScore ~= 1 then
+		io.write("You have " .. scores.playerScore .. " points, and the AI has " .. scores.aiScore .. " points. \n")
+	end
+end
 
 local function checkScores()
   if scores.playerScore > scores.aiScore then
@@ -77,11 +93,19 @@ local function askPlayer()
       played = 1
     end
     start = 1
+	elseif loweransw == "score" then
+		displayScores()
+		played = 1
+		start = 1
   end
 end
 
 while start == 1 do
   start = 0
+	if info == 0 then
+		info = 1
+		io.write("Some information about the game: This is a simple rock paper scissors project that I made for fun (Though I am still updating it). You can say 'score' at any time to display your \nscore as well as the AI's. I am planning on adding more features such as ASCII art so stay\ntuned!\n \n")
+	end
   if played == 0 then
     io.write("Do you want to play the game? \n ")
   else
@@ -93,7 +117,11 @@ while start == 1 do
     askPlayer()
   elseif loweransw == "no" then
     io.write("Goodbye!")
-  end
+  elseif loweransw == "score" then
+		displayScores()
+		played = 1
+		start = 1
+	end
 end
 
 local function start()
@@ -102,5 +130,5 @@ local function start()
   local loweransw = answ:lower()
   if loweransw == "yes" then
     askPlayer()
-  end
+	end
 end
